a7383afcdce6ac45d227a16129260ea68cc5b434
valenlopez14 <lopezvalen141@gmail.com>
2024-04-12 12:28:48 -0300

    ad

--- Clase3/fs.mjs
+++ Clase3/fs.mjs
@@ -1,4 +1,4 @@
-import { readFile } from "node:fs";
+import { readFile, mkdir} from "node:fs";
 import {sep, join, parse} from 'node:path'
 
 //Construimos la Ruta
@@ -8,14 +8,13 @@ readFile(ruta, (error,data) => {
     if(error) throw error;
     const datos = data.toString();
     const objeto = JSON.parse(datos)
-    console.log(objeto.productos);
+    // console.log(objeto.productos);
 });
 
-
-
-
-
-
-//git remote add origin https://github.com/Valenlopez14/asdsadprueba.git
-//git branch -M main
-//git push -u origin main
+//Crear un directorio 
+//Recursivees para que si no existe carpeta1, la cree igual y a su vez le almacene carpeta2 adentro
+//sin recursive:true da error ya que no puede crear carpeta 2 xq carpeta1 no existe.
+const ruta2 = join('carpeta1', 'carpeta2');
+mkdir(ruta2,{recursive: true},(err)=>{
+    if(err) throw err;
+});
